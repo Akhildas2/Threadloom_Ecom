575aeb0646d4a6acc892fd90a4a8fffb260d130e
Akhildas2 <akhildasaki12@gmail.com>
2024-04-24 20:01:38 +0530

    seventeen

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -1,18 +1,52 @@
 const Coupon = require('../models/couponModel')
 
-const addCoupon= async (req,res)=>{
+
+
+const listCoupon = async (req, res) => {
+    try {
+        const coupons = await Coupon.find({})
+        res.render('listCoupon', { coupons })
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+
+
+
+//for loading the add coupon page 
+const addCoupon = async (req, res) => {
     try {
         res.render('addCoupon')
     } catch (error) {
         console.log(error.message);
-        res.status(500).send('Internal Server Error'); 
+        res.status(500).send('Internal Server Error');
     }
 }
 
+
+
+//for adding coupon
 const insertCoupon = async (req, res) => {
     try {
         const { couponName, couponCode, discount, expiryDate, criteriaAmount } = req.body;
-        console.log("req",req.body)
+
+        const existingCoupon = await Coupon.findOne({
+            $or: [
+                { couponName: { $regex: new RegExp(couponName, 'i') } },
+                { couponCode: { $regex: new RegExp(couponCode, 'i') } },
+
+            ]
+        });
+
+
+        if (existingCoupon) {
+            // If a coupon with the same couponName or couponCode already exists, return an error
+            return res.status(400).json({ success: false, message: 'Coupon name or Coupon code already exists' });
+        }
+
         const newCoupon = new Coupon({
             couponName,
             couponCode,
@@ -22,24 +56,78 @@ const insertCoupon = async (req, res) => {
         });
 
         // Save the new coupon to the database
-       const a= await newCoupon.save();
-       console.log("a",a)
+        await newCoupon.save();
 
-        // Send a response back to the client
-        res.status(200).send('Coupon added successfully!');
+        return res.status(200).json({ success: true, url: '/admin/coupon/listcoupon' });
     } catch (error) {
-        console.log(error.message);
-        res.status(500).send('Internal Server Error');
+        console.error(error);
+        return res.status(500).json({ success: false, message: 'Internal Server Error. Please try again later.' });
     }
 };
 
+//for delete coupon 
+const deleteCoupon = async (req, res) => {
+    try {
+        const { couponId } = req.params;
+        const result = await Coupon.deleteOne({ _id: couponId });
+        if (result.deletedCount === 1) {
+            return res.status(200).json({ success: true });
+        } else {
+            return res.status(404).json({ success: false, message: 'Coupon not found.' });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: 'Internal Server Error. Please try again later.' });
+    }
+}
 
 
+//for editing the coupon 
+const editcoupon = async (req, res) => {
+    try {
+        console.log("entered")
+        const { couponId } = req.params;
+        console.log(" req.params", req.params)
+        const { couponName, couponCode, discount, expiryDate, criteriaAmount } = req.body;
+        console.log(" req.body", req.body)
+
+        const existingCoupon = await Coupon.findOne({
+            $and: [
+                { $or: [{ couponName: { $regex: new RegExp('^' + couponName + '$', 'i') } }, { couponCode: { $regex: new RegExp('^' + couponCode + '$', 'i') } }] },
+                { _id: { $ne: couponId } }
+            ]
+        });
 
 
+        console.log("existingCoupon", existingCoupon)
+        if (existingCoupon) {
+            return res.status(400).json({ success: false, message: 'A coupon with the same name or same coupon code already exists ' });
+        }
+
+        const coupon = await Coupon.findById(couponId);
+        if (!coupon) {
+            return res.status(404).json({ success: false, message: 'Coupon not found' });
+        }
+
+        coupon.couponName = couponName;
+        coupon.couponCode = couponCode;
+        coupon.discount = discount;
+        coupon.expiryDate = expiryDate;
+        coupon.criteriaAmount = criteriaAmount;
+        await coupon.save();
+
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: 'Internal Server Error. Please try again later.' });
+    }
+}
 
 
-module.exports={
+module.exports = {
+    listCoupon,
     addCoupon,
-    insertCoupon
+    insertCoupon,
+    deleteCoupon,
+    editcoupon
 }
--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -8,7 +8,12 @@ couponRoute.use(express.static('admin'));
 couponRoute.set('view engine', 'ejs');
 couponRoute.set('views', './views/admin');
 
+
+couponRoute.get('/listCoupon', adminAuth.isLogin, couponController.listCoupon);
 couponRoute.get('/addCoupon', adminAuth.isLogin, couponController.addCoupon);
 couponRoute.post('/addCoupon', adminAuth.isLogin, couponController.insertCoupon);
+couponRoute.delete('/deleteCoupon/:couponId', adminAuth.isLogin, couponController.deleteCoupon);
+couponRoute.put('/editCoupon/:couponId', adminAuth.isLogin, couponController.editcoupon);
+
 
 module.exports = couponRoute;
