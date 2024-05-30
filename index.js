99f9d82a095d8b01306412c4e4915a42fa86d1c6
Akhildas2 <akhildasaki12@gmail.com>
2024-05-30 23:31:34 +0530

    invoice design edit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { configurePayPal } = require('./config/paypalConfig');
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-      await mongoose.connect(process.env.MONGO_URI);
+      await mongoose.connect(process.env.MONGODB_URL);
       console.log('MongoDB connected...');
   } catch (err) {
       console.error('MongoDB connection error:', err);
