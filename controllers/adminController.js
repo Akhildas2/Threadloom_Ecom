2df4e49dae0433e8ba3714d33ff088b7f0e83eae
Akhildas2 <akhildasaki12@gmail.com>
2024-06-05 11:11:39 +0530

    last 12

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -209,7 +209,7 @@ const loadAdminHome = async (req, res) => {
 
 
 
-        res.render('adminHome', {
+        res.render('adminhome', {
             revenue,
             totalUsers,
             totalOrders,
