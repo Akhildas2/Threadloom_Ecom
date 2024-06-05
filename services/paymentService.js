1bf8ca6fed613b009e512581d3112cabfe8195d9
Akhildas2 <akhildasaki12@gmail.com>
2024-06-05 12:59:53 +0530

    payment fixed issue

--- services/paymentService.js
+++ services/paymentService.js
@@ -1,28 +1,27 @@
+
 const paypal = require('paypal-rest-sdk');
 const { truncateDescription } = require('../utils/orderHelper');
 require("dotenv").config();
 
 const createPayPalPayment = async (orderId, items, exchangeRate) => {
+    
+
     // Prepare item list for PayPal
     const itemList = items.flatMap(item => {
         return item.products.map(productDetail => ({
             "name": productDetail.productId.name,
             "description": truncateDescription(productDetail.productId.description),
             "quantity": productDetail.quantity,
-            "price": (productDetail.price * exchangeRate).toFixed(2), // Round the price to 2 decimal places for display
+            "price": (productDetail.price * exchangeRate).toFixed(2),
             "currency": "USD"
         }));
     });
-
-    // Calculate the total amount for all items without rounding
+    // Calculate the total amount for all items
     const itemTotal = items.reduce((total, item) => {
         return total + item.products.reduce((subtotal, productDetail) => {
             return subtotal + (productDetail.price * productDetail.quantity * exchangeRate);
         }, 0);
-    }, 0);
-
-    // Round the total amount to 2 decimal places for display
-    const roundedItemTotal = itemTotal.toFixed(2);
+    }, 0).toFixed(2);
 
     const create_payment_json = {
         "intent": "sale",
@@ -39,7 +38,7 @@ const createPayPalPayment = async (orderId, items, exchangeRate) => {
             },
             "amount": {
                 "currency": "USD",
-                "total": roundedItemTotal // Use the rounded total here
+                "total": itemTotal
             },
             "description": "Order summary of the product."
         }],
@@ -48,7 +47,6 @@ const createPayPalPayment = async (orderId, items, exchangeRate) => {
             brand_name: "threadloom"
         }
     };
-
     return new Promise((resolve, reject) => {
         paypal.payment.create(create_payment_json, (error, payment) => {
             if (error) {
